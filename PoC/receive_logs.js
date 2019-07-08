9d9f9f9b333120861bba397ed1d3ec4644f7f87b
lwirtz1 <lena.wirtz@th-koeln.de>
2019-07-08 18:19:19 +0200

    ersten PoC für RabbitMQ gecodet

---
+++ PoC/receive_logs.js
@@ -0,0 +1,84 @@
+
+/*
+//jede Nachricht von emit_logs.js get an den exchanger, der schickt es an alle Queues und diese an alle Receiver
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var exchange = 'logs';
+
+    channel.assertExchange(exchange, 'fanout', {
+      durable: false
+    });
+
+    channel.assertQueue('', {
+      exclusive: true
+    }, function(error2, q) {
+      if (error2) {
+        throw error2;
+      }
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      channel.bindQueue(q.queue, exchange, '');
+
+      channel.consume(q.queue, function(msg) {
+        if(msg.content) {
+            console.log(" [x] %s", msg.content.toString());
+          }
+      }, {
+        noAck: true
+      });
+    });
+  });
+});
+*/
+
+var amqp = require('amqplib/callback_api');
+
+var args = ['info','warning'];
+//process.argv.slice(2);
+
+if (args.length == 0) {
+  console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+  process.exit(1);
+}
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var exchange = 'direct_logs';
+
+    channel.assertExchange(exchange, 'direct', {
+      durable: false
+    });
+
+    channel.assertQueue('', {
+      exclusive: true
+      }, function(error2, q) {
+        if (error2) {
+          throw error2;
+        }
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+      args.forEach(function(severity) {
+        channel.bindQueue(q.queue, exchange, severity);
+      });
+
+      channel.consume(q.queue, function(msg) {
+        console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+      }, {
+        noAck: true
+      });
+    });
+  });
+});
