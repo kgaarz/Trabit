7c763f4e6f3fe9aeb73a0192088781313287fda9
kgaarz <konstantin.gaarz@smail.th-koeln.de>
2019-07-31 14:41:41 +0200

    Directions-Request parametrisiert

--- TeamMK/PoC/server/routes/directions.js
+++ TeamMK/PoC/server/routes/directions.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const axios = require('axios');
+const router = express.Router();
 require('dotenv/config');
 
-const router = express.Router();
-
 router.get('/', (req, res) => {
-
-axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyCZVkrJkiRNaIS3f60yWWKRNjdfGbdGayI')
-  .then(response => {
-    console.log(response.data);
-	res.send(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  var origin = 'Disneyland';
+  var destination = 'Universal+Studios+Hollywood';
+  axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&key=' +
+      process.env.ROADS_KEY)
+    .then(response => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
+router.post('/', (req, res) => {
+  var mode = req.body.mode ? req.body.mode: 'driving';
 
+  axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + req.body.origin + '&destination=' + req.body.destination +
+  '&mode=' + mode + '&key=' + process.env.ROADS_KEY)
+    .then(response => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
