f964abfff5f076fc04964479d3b1d33907612825
kgaarz <konstantin.gaarz@smail.th-koeln.de>
2019-08-01 14:07:08 +0200

    Hinzufügen der Query Parameter zum Get-Request der Directions API

--- TeamMK/PoC/server/routes/directions.js
+++ TeamMK/PoC/server/routes/directions.js
@@ -4,10 +4,10 @@ const router = express.Router();
 require('dotenv/config');
 
 router.post('/', (req, res) => {
-  var mode = req.body.mode ? req.body.mode: 'driving';
+  var mode = req.body.mode ? req.body.mode : 'driving';
 
   axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + req.body.origin + '&destination=' + req.body.destination +
-  '&mode=' + mode + '&key=' + process.env.DIRECTIONS_KEY)
+      '&mode=' + mode + '&departure_time=' + req.body.departure_time + '&key=' + process.env.DIRECTIONS_KEY)
     .then(response => {
       console.log(response.data);
       res.send(response.data);
@@ -17,4 +17,37 @@ router.post('/', (req, res) => {
     });
 });
 
+router.get('/', (req, res) => {
+  if (req.query.origin && req.query.destination) {
+
+    var mode = req.query.mode ? req.query.mode : 'driving';
+    var now;
+
+    if (!req.query.departure_time) {
+      now = new Date().getTime();
+    } else {
+      console.log(req.query.departure_time);
+      console.log(new Date().getTime());
+      now = req.query.departure_time > new Date().getTime() ? req.query.departure_time : res.send("Ungültige Abfahrtszeit");
+    }
+
+    axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + req.query.origin + '&destination=' + req.query.destination +
+        '&mode=' + mode + '&departure_time=' + now + '&key=' + process.env.DIRECTIONS_KEY)
+      .then(response => {
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    res.send("Gib Start & Ziel ein!");
+  }
+});
+
 module.exports = router;
+
+
+
+//50.941357, 6.958307
+//51.023180, 7.562370
