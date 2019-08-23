2f6b65b6f243e007c1e5d557e51cd196d7d322c6
Jarera <julian.arera@arcor.de>
2019-08-23 14:07:34 +0200

    carAndBikeHelper um Alternativen erweitert

--- TeamMK/MVP/controllers/helpers/options/carAndBikeHelper.js
+++ TeamMK/MVP/controllers/helpers/options/carAndBikeHelper.js
@@ -1,37 +1,46 @@
 var apiRequestHelper = require("../apiRequestHelper");
+const getSortedRoutesHelper = require('../getSortedRoutesHelper');
 
 module.exports = function(origin, destination, departureTime) {
-    return new Promise(function(resolve, reject) {
-        var carWay = apiRequestHelper.getGoogleDirectionsAPIData(origin, destination, departureTime, "driving");
-        var bikeWay = apiRequestHelper.getGoogleDirectionsAPIData(origin, destination, departureTime, "bicycling");
+  return new Promise(function(resolve, reject) {
+      var carWay = apiRequestHelper.getGoogleDirectionsAPIDataWithAlternatives(origin, destination, departureTime, "driving");
+      var bikeWay = apiRequestHelper.getGoogleDirectionsAPIDataWithAlternatives(origin, destination, departureTime, "bicycling");
 
-        Promise.all([carWay, bikeWay]).then((values) => {
-            var shortestRoute;
-            if(values[0].duration<values[1].duration){
-                shortestRoute = values[0];
-                mode = "driving";
-            }else{
-                shortestRoute = values[1];
-                mode = "bicycling";
-            }
+      Promise.all([carWay, bikeWay]).then((values) => {
+          var routes = [];
 
+          for (i = 0; i < values[0].length; i++) {
             const selectionOption = {
-                modes: mode,
-                duration: shortestRoute.duration,
-                distance: shortestRoute.distance,
-                switches: 0,
-                sustainability: 0,
-                route: shortestRoute
-              }
-              resolve(selectionOption);
+              modes: "driving",
+              duration: values[0][i].duration,
+              distance: values[0][i].distance,
+              switches: 0,
+              sustainability: 0,
+              route: values[0][i]
+            }
+            routes.push(selectionOption);
+          }
+          for (i = 0; i < values[1].length; i++) {
+            const selectionOption = {
+              modes: "bicycling",
+              duration: values[1][i].duration,
+              distance: values[1][i].distance,
+              switches: 0,
+              sustainability: 0,
+              route: values[1][i]
+            }
+            routes.push(selectionOption);
+          }
 
-          },
-          (error)=>{
-            reject(error);
-          });
+          resolve(getSortedRoutesHelper(routes));
+
         },
-        (error)=>{
+        (error) => {
           reject(error);
         });
+    },
+    (error) => {
+      reject(error);
+    });
 
 }
