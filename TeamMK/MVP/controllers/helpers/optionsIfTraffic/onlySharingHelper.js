fabeb348d5d6fe045ba5e6c2102ea70acb517f72
Jarera <julian.arera@arcor.de>
2019-09-12 14:58:06 +0200

    Komplexität der Routengenerierung von onlySharing aufgrund der HereDirectionsAPI verringert

--- TeamMK/MVP/controllers/helpers/optionsIfTraffic/onlySharingHelper.js
+++ TeamMK/MVP/controllers/helpers/optionsIfTraffic/onlySharingHelper.js
@@ -7,43 +7,24 @@ module.exports = function(incidents, origin, destination, departureTime) {
   return new Promise(function(resolve, reject) {
       var cabData = [];
       var checkCabData = false;
-      var smallRadiusBike = apiRequestHelper.getCabData(origin.lat, origin.lng, 200);
-      var mediumRadiusBike = apiRequestHelper.getCabData(origin.lat, origin.lng, 400);
       var bigRadiusBike = apiRequestHelper.getCabData(origin.lat, origin.lng, 1000);
 
       var flinksterData = [];
       var checkFlinksterData = false;
-      var smallRadiusCar = apiRequestHelper.getFlinksterData(origin.lat, origin.lng, 500);
-      var mediumRadiusCar = apiRequestHelper.getFlinksterData(origin.lat, origin.lng, 1250);
       var bigRadiusCar = apiRequestHelper.getFlinksterData(origin.lat, origin.lng, 2500);
 
-      Promise.all([smallRadiusBike, mediumRadiusBike, bigRadiusBike, smallRadiusCar, mediumRadiusCar, bigRadiusCar]).then((values) => {
+      Promise.all([bigRadiusBike, bigRadiusCar]).then((values) => {
             if (values[0].length == 0) {
-              if (values[1].length == 0) {
-                if (values[2].length == 0) {
-                  checkCabData = true;
-                } else {
-                  cabData = values[2];
-                }
-              } else {
-                cabData = values[1];
-              }
+              checkCabData = true;
             } else {
               cabData = values[0];
             }
-            if (values[3].length == 0) {
-              if (values[4].length == 0) {
-                if (values[5].length == 0) {
-                  checkFlinksterData = true;
-                } else {
-                  flinksterData = values[5];
-                }
-              } else {
-                flinksterData = values[4];
-              }
+            if (values[1].length == 0) {
+              checkFlinksterData = true;
             } else {
-              flinksterData = values[3];
+              flinksterData = values[1];
             }
+
             if (checkCabData && checkFlinksterData) {
               resolve(false);
             }
@@ -54,8 +35,11 @@ module.exports = function(incidents, origin, destination, departureTime) {
             reject(error);
           })
         .then((result) => {
-          console.log("FINALES RESULT: " + result);
-            resolve(result[0]);
+            if (result[0].duration) {
+              resolve(result[0]);
+            } else {
+              resolve(false);
+            }
           },
           (error) => {
             reject(error);
@@ -70,14 +54,17 @@ function checkNearSharingRoutes(incidents, cabData, flinksterData, origin, desti
   return new Promise(function(resolve, reject) {
 
       totalRoutes = [];
-      for (i = 0; i < cabData.length; i++) {
+      for (i = 0;
+        (i < cabData.length) && (i < 3); i++) {
         totalRoutes.push(createWalkingBikeRoute(incidents, cabData, origin, destination, departureTime, i));
-        for (j = 0; j < flinksterData.length; j++) {
+        for (j = 0;
+          (j < flinksterData.length) && (j < 3); j++) {
           totalRoutes.push(createWalkingBikeCarRoute(incidents, cabData, flinksterData, origin, destination, departureTime, i, j));
         }
       };
 
-      for (i = 0; i < flinksterData.length; i++) {
+      for (i = 0;
+        (i < flinksterData.length) && (i < 3); i++) {
         totalRoutes.push(createWalkingCarRoute(incidents, flinksterData, origin, destination, departureTime, i));
       };
 
@@ -139,29 +126,33 @@ function createWalkingBikeCarRoute(incidents, cabData, flinksterData, origin, de
       var carWay = apiRequestHelper.getHereDirectionsAPIData(flinksterData[j].geoLocation, destination, "car", incidents);
 
       Promise.all([walkingWay, bikeWay, carWay]).then((values) => {
-          totalRoute = {
-            distance: values[0].distance + values[1].distance + values[2].distance,
-            duration: values[0].duration + values[1].duration + values[2].duration,
-            startLocation: {
-              lat: origin.lat,
-              lng: origin.lng
-            },
-            endLocation: {
-              lat: destination.lat,
-              lng: destination.lng
-            },
-            steps: values[0].steps.concat(values[1].steps.concat(values[2].steps))
-          }
+          if (values[0] && values[1] && values[2]) {
+            totalRoute = {
+              distance: values[0].distance + values[1].distance + values[2].distance,
+              duration: values[0].duration + values[1].duration + values[2].duration,
+              startLocation: {
+                lat: origin.lat,
+                lng: origin.lng
+              },
+              endLocation: {
+                lat: destination.lat,
+                lng: destination.lng
+              },
+              steps: values[0].steps.concat(values[1].steps.concat(values[2].steps))
+            }
 
-          const selectionOption = {
-            modes: ["walking", "bicycling", "driving"],
-            duration: totalRoute.duration,
-            distance: totalRoute.distance,
-            switches: 2,
-            sustainability: generateSustainabilityScoreHelper(totalRoute.steps),
-            route: totalRoute
+            const selectionOption = {
+              modes: ["walking", "bicycling", "driving"],
+              duration: totalRoute.duration,
+              distance: totalRoute.distance,
+              switches: 2,
+              sustainability: generateSustainabilityScoreHelper(totalRoute.steps),
+              route: totalRoute
+            }
+            resolve(selectionOption);
+          } else {
+            resolve(false);
           }
-          resolve(selectionOption);
         },
         (error) => {
           reject(error);
