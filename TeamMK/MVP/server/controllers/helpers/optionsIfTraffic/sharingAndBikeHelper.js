2b77ac89c00bb854aebd272a1a528295936fc60c
Jarera <julian.arera@arcor.de>
2019-09-14 14:17:05 +0200

    sharingAndCar und sharingAndBikeHelper implementiert

--- TeamMK/MVP/server/controllers/helpers/optionsIfTraffic/sharingAndBikeHelper.js
+++ TeamMK/MVP/server/controllers/helpers/optionsIfTraffic/sharingAndBikeHelper.js
@@ -1,42 +1,26 @@
-const apiRequestHelper = require('../apiRequestHelper');
 const onlySharingHelper = require('./onlySharingHelper');
 const onlyBikeHelper = require('./onlyBikeHelper');
 const getSortedRoutesHelper = require('../getSortedRoutesHelper');
-const checkNearRoutesHelper = require('../checkNearRoutesHelper');
 
-
-module.exports = function(origin, destination, departureTime) {
+module.exports = function(incidents, origin, destination, departureTime) {
   return new Promise(function(resolve, reject) {
-    var flinksterData = [];
-    var smallRadiusCar = apiRequestHelper.getFlinksterData(origin.lat, origin.lng, 500);
-    var mediumRadiusCar = apiRequestHelper.getFlinksterData(origin.lat, origin.lng, 1250);
-    var bigRadiusCar = apiRequestHelper.getFlinksterData(origin.lat, origin.lng, 2500);
 
-    Promise.all([smallRadiusCar, mediumRadiusCar, bigRadiusCar]).then((values) => {
-          if (values[0].length == 0) {
-            if (values[1].length == 0) {
-              if (values[2].length == 0) {
-                reject("error: No Carsharing found");
-              } else {
-                flinksterData = values[2];
-              }
-            } else {
-              flinksterData = values[1];
-            }
-          } else {
-            flinksterData = values[0];
-          }
-          return checkNearRoutesHelper.checkNearRoutesForTwo(flinksterData, onlySharingHelper, onlyBikeHelper, origin, destination, departureTime, "bicycling", "driving");
-        },
-        (error) => {
-          reject(error);
-        })
-      .then((result) => {
-          resolve(result);
-        },
-        (error) => {
-          reject(error);
-        });
+    var onlySharing = onlySharingHelper(incidents, origin, destination, departureTime);
+    var onlyBike = onlyBikeHelper(incidents, origin, destination);
+
+    Promise.all([onlySharing, onlyBike]).then((values) => {
+        var result = [];
+
+        for(i=0; i<values.length; i++){
+            result.push(values[i]);
+        }
+
+        var sorted = getSortedRoutesHelper(result);
+        resolve(sorted[0]);
+      },
+      (error) => {
+        reject(error);
+      });
+
   });
-
 }
--- TeamMK/MVP/server/controllers/helpers/optionsIfTraffic/sharingAndCarHelper.js
+++ TeamMK/MVP/server/controllers/helpers/optionsIfTraffic/sharingAndCarHelper.js
@@ -2,22 +2,21 @@ const onlySharingHelper = require('./onlySharingHelper');
 const onlyCarHelper = require('./onlyCarHelper');
 const getSortedRoutesHelper = require('../getSortedRoutesHelper');
 
-module.exports = function(origin, destination, departureTime) {
+module.exports = function(incidents, origin, destination, departureTime) {
   return new Promise(function(resolve, reject) {
 
-    var onlySharing = onlySharingHelper(origin, destination, departureTime);
-    var onlyCar = onlyCarHelper(origin, destination, departureTime);
+    var onlySharing = onlySharingHelper(incidents, origin, destination, departureTime);
+    var onlyCar = onlyCarHelper(incidents, origin, destination);
 
     Promise.all([onlySharing, onlyCar]).then((values) => {
         var result = [];
-        
-        for(i=0; i<values.length; i++){
-          for(j=0; j<values[i].length; j++){
-            result.push(values[i][j]);
-          }
-        }
 
-        resolve(getSortedRoutesHelper(result));
+        for(i=0; i<values.length; i++){
+            result.push(values[i]);
+        }
+        
+        var sorted = getSortedRoutesHelper(result);
+        resolve(sorted[0]);
       },
       (error) => {
         reject(error);
