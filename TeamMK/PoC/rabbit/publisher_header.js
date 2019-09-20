13ee8fc27810f0411aac096a4de1e482c1572204
lwirtz1 <lena-wirtz@t-online.de>
2019-09-20 18:58:55 +0200

    Publisher und Receiver für den Traffic angepasst

--- TeamMK/PoC/rabbit/publisher_header.js
+++ TeamMK/PoC/rabbit/publisher_header.js
@@ -1,4 +1,4 @@
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', (error0, connection)=> {
   if (error0) {
@@ -13,26 +13,31 @@ amqp.connect('amqp://localhost', (error0, connection)=> {
     var rountingKey= "Warning";
 
     var headerOptions = new Map();
-    var headerKey = "2";
-    var headerValue = {lat:"51.941357", lng: "7.958307"}
+    var headerKey = "trafficCord";
+    var headerValue = {lat:"51.941357", lng: "7.958307"};
+    var headerValue2 = {lat:"50.941357", lng: "6.958307"};
     headerOptions.set(headerKey, headerValue);
-  
+    headerOptions.set("headerKey", headerValue2);
+    var opts = [];
+    headerOptions.forEach((value,key)=> {
+        let store = {'key': key, 'value': value};
+        opts.push({headers:store});
+      });
+    console.log(opts.length);
+
     channel.assertExchange(exchange, 'headers', {
       durable: false
     });
 
-    let opts = {headers: { 'key': headerKey, 'value': headerValue}};
-    channel.publish(exchange, rountingKey, Buffer.from(msg), opts);
-    console.log("Sent Header:"+headerKey+"; "+headerValue.lat+"-"+ headerValue.lng+ " and Message: " +msg);
-
+    //let opts = [{headers: {'key': headerKey, 'value': headerValue}}, {headers:{'key': "test", 'value': headerValue2} }];
+    for(i=0; i<opts.length; i++){
+    channel.publish(exchange, rountingKey, Buffer.from(msg), opts[i]);
+    console.log("Sent Header:"+opts[i].headers.key+"; "+opts[i].headers.value.lat+"-"+ opts[i].headers.value.lng+ " and Message: " +msg);
+}
   });
 
-  setTimeout(()=> { 
-    connection.close(); 
-    process.exit(0) 
-  }, 500);
+  setTimeout(()=> {
+    connection.close();
+    process.exit(0)
+  }, 10000);
 });
-
-
-
-  
--- TeamMK/PoC/rabbit/receiver_header.js
+++ TeamMK/PoC/rabbit/receiver_header.js
@@ -1,12 +1,15 @@
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
 var headerOptions = new Map();
 var location = {lat:"50.941357", lng: "6.958307"};
 var location2 = {lat:"51.941357", lng: "7.958307"};
-headerOptions.set("1", location);
-headerOptions.set("2", location2);
-headerOptions.set("x-match", "all");
+
+//INFO: unterschiedliche Keys erforderlich
+headerOptions.set("trafficCord2", location);
+headerOptions.set("trafficCord", location2);
+headerOptions.set("x-match", "any");
+console.log(headerOptions);
 
 amqp.connect('amqp://localhost', (error0, connection) => {
   if (error0) {
@@ -23,17 +26,17 @@ amqp.connect('amqp://localhost', (error0, connection) => {
     });
 
     channel.assertQueue('', {
-      exclusive: true
+      durable: true,
+      exclusive: false
     }, (error2, q) =>{
       if (error2) {
         throw error2;
       }
       console.log(' [*] Waiting for logs. To exit press CTRL+C');
-      
+
       headerOptions.forEach((value,key)=> {
         let opts = {'key': key, 'value': value};
         channel.bindQueue(q.queue, exchange, '', opts);
-
         console.log("Key: "+key + " ValueLat: " + value.lat+" ValueLng: "+value.lng );
       },headerOptions);
 
@@ -41,10 +44,7 @@ amqp.connect('amqp://localhost', (error0, connection) => {
         console.log("%s: %s bei: %s/%s", msg.fields.routingKey, msg.content.toString(), msg.properties.headers.value.lat,msg.properties.headers.value.lng );
       }, {
         noAck: true
-        //exclusive: false
-  
       });
     });
   });
 });
-
