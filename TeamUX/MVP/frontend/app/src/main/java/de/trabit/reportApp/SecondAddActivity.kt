dc0b06190750a3eabe54f29148464bd81b2656b4
Julia Pritzen <julia.pritzen@gmail.com>
2019-09-23 15:23:19 +0200

    fixed typos in variable names, added error snackbar, used correct value for suyway transport.tag, added id for main linearLayout

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/SecondAddActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/SecondAddActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-
+import kotlinx.android.synthetic.main.activity_second_add.*
 
 class SecondAddActivity : AppCompatActivity() {
 
@@ -12,76 +12,58 @@ class SecondAddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_add)
 
-        //store the value (choosen means of transport) in a variable
-
-        val choosenMeansOfTransport = intent.getStringExtra("meansOfTransport")
+        //store the value (chosen means of transport) in a variable
+        val chosenMeansOfTransport = intent.getStringExtra("meansOfTransport")
 
         //Adapt the activity layout to the appropriate means of transport
+        val imageMeansOfTransport = findViewById<ImageView>(R.id.imageMeansOftransport)
+        val textMeansOfTransport = findViewById<TextView>(R.id.textMeansOfTransport)
 
-        val imageMeansOftransport = findViewById(R.id.imageMeansOftransport) as ImageView
-        val textMeansofTransport = findViewById(R.id.textMeansOfTransport) as TextView
-
-        when (choosenMeansOfTransport) {
-
+        when (chosenMeansOfTransport) {
             "bus"   -> {
-                imageMeansOftransport.setImageResource(R.mipmap.bus_icon_clicked)
-                textMeansofTransport.setText("Bus")
+                imageMeansOfTransport.setImageResource(R.mipmap.bus_icon_clicked)
+                textMeansOfTransport.text = "Bus"
             }
             "train" -> {
-            imageMeansOftransport.setImageResource(R.mipmap.train_icon_clicked)
-            textMeansofTransport.setText("Zug")
+                imageMeansOfTransport.setImageResource(R.mipmap.train_icon_clicked)
+                textMeansOfTransport.text = "Zug"
                 }
-            "tram"  -> {
-                imageMeansOftransport.setImageResource(R.mipmap.tram_icon_clicked)
-                textMeansofTransport.setText("Bahn")
+            "subway"  -> {
+                imageMeansOfTransport.setImageResource(R.mipmap.tram_icon_clicked)
+                textMeansOfTransport.text = "U-Bahn / Tram"
             }
         }
 
         //close Activity by clicking the x and go back to OverviewActivity
-
-        val closeButton = findViewById(R.id.closeSecondAddButton) as ImageButton
-
+        val closeButton = findViewById<ImageButton>(R.id.closeSecondAddButton)
         closeButton.setOnClickListener{
-
             val closeIntent = Intent(this,OverviewActivity::class.java)
             startActivity(closeIntent)
         }
 
-        //back Button Intent to FirstAddActivtiy
-
-        val backBtnToFirstAddActivity = findViewById(R.id.btn_back) as ImageButton
-
+        //back Button Intent to FirstAddActivity
+        val backBtnToFirstAddActivity = findViewById<ImageButton>(R.id.btn_back)
         backBtnToFirstAddActivity.setOnClickListener{
             val backIntent = Intent(this,FirstAddActivity::class.java)
             startActivity(backIntent)
         }
 
         //read out user input from edittext (Id of Means of Transport)
-
-        val meansOfTransportIdButton = findViewById(R.id.inputTextNr) as EditText
-        val meansOfTansportId = meansOfTransportIdButton.getText()
-        val confirmBtn = findViewById(R.id.btn_next) as Button
+        val meansOfTransportIdButton = findViewById<EditText>(R.id.inputTextNr)
+        val meansOfTransportId = meansOfTransportIdButton.text
+        val confirmBtn = findViewById<Button>(R.id.btn_next)
 
         //add intent to further button to hand off to SecondAddActivity, checking if something is written in the edittext field
-
         confirmBtn.setOnClickListener {
-            if(meansOfTansportId.isEmpty() || meansOfTansportId.length== 0 || meansOfTansportId.equals("") || meansOfTansportId == null){
-                val errorToast = Toast.makeText(
-                    this@SecondAddActivity,
-                    "Bitte gib zunächst eine Kennung ein.",
-                    Toast.LENGTH_SHORT )
-                    errorToast.show()
-
+            if(meansOfTransportId.isNullOrBlank()){
+                ErrorSnackbar(linearLayout_secondAdd).show("Bitte gebe eine Kennung ein!")
             } else {
-                val lastAddActivtityIntent = Intent(this, ThirdAddActivity::class.java)
-                lastAddActivtityIntent.putExtra("meansOfTransport", choosenMeansOfTransport)
-                lastAddActivtityIntent.putExtra("meansOfTansportId", meansOfTansportId)
-                startActivity(lastAddActivtityIntent)
-
+                val lastAddActivityIntent = Intent(this, ThirdAddActivity::class.java)
+                lastAddActivityIntent.putExtra("meansOfTransport", chosenMeansOfTransport)
+                lastAddActivityIntent.putExtra("meansOfTransportId", meansOfTransportId.toString())
+                startActivity(lastAddActivityIntent)
             }
-
         }
-
     }
 }
 
