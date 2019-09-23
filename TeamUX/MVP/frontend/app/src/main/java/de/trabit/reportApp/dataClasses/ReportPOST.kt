c72d21f99e1ab59c8d637bc3f30ab6ade6b4cf8d
Julia Pritzen <julia.pritzen@gmail.com>
2019-09-23 16:37:35 +0200

    added data class TransportPOST for handling optional transport tag parameter on report post request

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/dataClasses/ReportPOST.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/dataClasses/ReportPOST.kt
@@ -3,6 +3,6 @@ package com.example.api_test.dataClasses
 data class ReportPOST (val author : String,
                        val description : String,
                        val location : LocationObject,
-                       val transport : Transport,
+                       val transport : TransportPOST,
                        val metadata : Metadata = Metadata()) {
 }
---
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/dataClasses/TransportPOST.kt
@@ -0,0 +1,5 @@
+package com.example.api_test.dataClasses
+
+data class TransportPOST (val type : String,
+                          val tag : String?) {
+}
