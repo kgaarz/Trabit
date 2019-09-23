44f7fe11eed7939f987b195ad6769b6beffc41bd
Julia Pritzen <julia.pritzen@gmail.com>
2019-09-23 15:16:40 +0200

    switched position of train & subway (tram) buttons, changed tram to subway according to enum values in report schema

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/FirstAddActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/FirstAddActivity.kt
@@ -12,54 +12,48 @@ class FirstAddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_add)
 
-
         //close Activity by clicking the x and go to overviewActivity
-
-        val closeButton = findViewById(R.id.closeFirstAddButton) as ImageButton
+        val closeButton = findViewById<ImageButton>(R.id.closeFirstAddButton)
 
         closeButton.setOnClickListener{
-
             val closeIntent = Intent(this,OverviewActivity::class.java)
             startActivity(closeIntent)
         }
 
-        //Intent to next Activity by clicking on the tiles. The choosen means of transport is stored and handed over to next activtiy
+        //Intent to next Activity by clicking on the tiles. The chosen means of transport is stored and handed over to next activity
+        val tramTile = findViewById<RelativeLayout>(R.id.tramTile)
+        val trainTile = findViewById<RelativeLayout>(R.id.trainTile)
+        val busTile = findViewById<RelativeLayout>(R.id.busTile)
+        val carTile = findViewById<RelativeLayout>(R.id.carTile)
 
-        val tramTile = findViewById(R.id.tramTile) as RelativeLayout
-        val trainTile = findViewById(R.id.trainTile) as RelativeLayout
-        val busTile = findViewById(R.id.busTile) as RelativeLayout
-        val carTile = findViewById(R.id.carTile) as RelativeLayout
-
-        var choosenMeansOfTransport : String
-
-        tramTile.setOnClickListener{
-            val nextAddActivtityIntent = Intent(this,SecondAddActivity::class.java)
-            choosenMeansOfTransport ="tram"
-            nextAddActivtityIntent.putExtra("meansOfTransport", choosenMeansOfTransport)
-            startActivity(nextAddActivtityIntent)
-        }
+        var chosenMeansOfTransport : String
 
         trainTile.setOnClickListener{
             val nextAddActivtityIntent = Intent(this,SecondAddActivity::class.java)
-            choosenMeansOfTransport ="train"
-            nextAddActivtityIntent.putExtra("meansOfTransport", choosenMeansOfTransport)
+            chosenMeansOfTransport ="train"
+            nextAddActivtityIntent.putExtra("meansOfTransport", chosenMeansOfTransport)
+            startActivity(nextAddActivtityIntent)
+        }
+
+        tramTile.setOnClickListener{
+            val nextAddActivtityIntent = Intent(this,SecondAddActivity::class.java)
+            chosenMeansOfTransport ="subway"
+            nextAddActivtityIntent.putExtra("meansOfTransport", chosenMeansOfTransport)
             startActivity(nextAddActivtityIntent)
         }
 
         busTile.setOnClickListener{
             val nextAddActivtityIntent = Intent(this,SecondAddActivity::class.java)
-            choosenMeansOfTransport ="bus"
-            nextAddActivtityIntent.putExtra("meansOfTransport", choosenMeansOfTransport)
+            chosenMeansOfTransport ="bus"
+            nextAddActivtityIntent.putExtra("meansOfTransport", chosenMeansOfTransport)
             startActivity(nextAddActivtityIntent)
         }
 
         carTile.setOnClickListener{
             val nextAddActivtityIntent = Intent(this,ThirdAddActivity::class.java)
-            choosenMeansOfTransport ="car"
-            nextAddActivtityIntent.putExtra("meansOfTransport", choosenMeansOfTransport)
+            chosenMeansOfTransport ="car"
+            nextAddActivtityIntent.putExtra("meansOfTransport", chosenMeansOfTransport)
             startActivity(nextAddActivtityIntent)
         }
-
-
     }
 }
