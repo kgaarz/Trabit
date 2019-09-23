d31bad87e5cb5191e4e96dd1756404b9d0921d38
kgaarz <konstantin.gaarz@smail.th-koeln.de>
2019-09-23 21:13:41 +0200

    Einbindung von Google Places Autocomplete zur Suche von Origin und Destination

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/DirectionsActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/DirectionsActivity.kt
@@ -2,14 +2,15 @@ package de.trabit.reportApp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.android.volley.Request
-
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import java.util.*
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
 
 
 class DirectionsActivity : AppCompatActivity() {
@@ -17,6 +18,8 @@ class DirectionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_directions)
+
+        initPlaces()
 
         val overviewIcon = findViewById(R.id.overviewNavigation) as ImageButton
         val profileIcon = findViewById(R.id.profileNavigation) as ImageButton
@@ -39,19 +42,61 @@ class DirectionsActivity : AppCompatActivity() {
         }
 
 
-        val textView = findViewById<TextView>(R.id.response)
+        val autocompleteFragmentOrigin = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment_origin) as AutocompleteSupportFragment?
+        val autocompleteFragmentDestination = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment_destination) as AutocompleteSupportFragment?
 
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://trabit-api-mk.herokuapp.com/traffics/5d5414fa043e699565a4795e"
+        if (autocompleteFragmentOrigin != null) {
+            autocompleteFragmentOrigin.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG))
+        }
 
-        val stringRequest = StringRequest(Request.Method.GET, url,
-            Response.Listener<String> { response ->
-                // Display the first 500 characters of the response string.
-                textView.text = "Response is: ${response.substring(0, 500)}"
-            },
-            Response.ErrorListener { textView.text = "That didn't work!" })
+        if (autocompleteFragmentOrigin != null) {
+            autocompleteFragmentOrigin.setOnPlaceSelectedListener(object: PlaceSelectionListener {
+                override fun onError(p0: Status) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
 
-        queue.add(stringRequest)
+                override fun onPlaceSelected(place: Place) {
+                    // TODO: Get info about the selected place.
+                    Log.i("TAG", "Place: " + place.latLng)
+                }
 
+                fun onError() {
+                    // TODO: Handle the error.
+                    Log.i("TAG", "An error occurred")
+                }
+            })
+        }
+
+        if (autocompleteFragmentDestination != null) {
+            autocompleteFragmentDestination.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG))
+        }
+
+        if (autocompleteFragmentDestination != null) {
+            autocompleteFragmentDestination.setOnPlaceSelectedListener(object: PlaceSelectionListener {
+                override fun onError(p0: Status) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onPlaceSelected(place: Place) {
+                    // TODO: Get info about the selected place.
+                    Log.i("TAG", "Place: " + place.latLng)
+                }
+
+                fun onError() {
+                    // TODO: Handle the error.
+                    Log.i("TAG", "An error occurred")
+                }
+            })
+        }
     }
+
+    fun initPlaces(){
+        // Initialize the SDK
+        Places.initialize(applicationContext, "AIzaSyCZVkrJkiRNaIS3f60yWWKRNjdfGbdGayI")
+
+        // Create a new Places client instance
+        val placesClient = Places.createClient(this)
+    }
+
 }
+
