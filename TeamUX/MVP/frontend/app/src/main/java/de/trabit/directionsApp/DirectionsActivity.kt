8f14958eefd46484f292f63e2e627552c4b9974e
kgaarz <konstantin.gaarz@smail.th-koeln.de>
2019-09-25 14:32:20 +0200

    DatePicker & TimePicker zum angeben der DepartureTime hinzugefügt

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/DirectionsActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/DirectionsActivity.kt
@@ -1,9 +1,13 @@
 package de.trabit.directionsApp
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.DatePicker
 import android.widget.ImageButton
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
@@ -16,9 +20,63 @@ import de.trabit.reportApp.R
 
 class DirectionsActivity : AppCompatActivity() {
 
+    var year: Int = 0
+    var month: Int= 0
+    var day: Int= 0
+    var hour: Int= 0
+    var minute: Int= 0
+    var timestamp: Long= 0
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_directions)
+
+        // Get current calendar date and time.
+        val currCalendar = Calendar.getInstance()
+
+        year = currCalendar.get(Calendar.YEAR)
+        month = currCalendar.get(Calendar.MONTH)
+        day = currCalendar.get(Calendar.DAY_OF_MONTH)
+        hour = currCalendar.get(Calendar.HOUR_OF_DAY)
+        minute = currCalendar.get(Calendar.MINUTE)
+        timestamp = currCalendar.getTimeInMillis();
+
+        showUserSelectDateTime()
+
+        // Get date picker object.
+        val datePicker = findViewById(R.id.datePicker) as DatePicker
+        datePicker.init(
+            year, month, day
+        ) { datePicker, year, month, day ->
+            this@DirectionsActivity.year = year
+            this@DirectionsActivity.month = month
+            this@DirectionsActivity.day = day
+
+            currCalendar.set(year, month, day, this@DirectionsActivity.hour, this@DirectionsActivity.minute)
+
+            this@DirectionsActivity.timestamp = currCalendar.getTimeInMillis()
+
+            showUserSelectDateTime()
+        }
+
+        // Get time picker object.
+        val timePicker = findViewById(R.id.timePicker) as TimePicker
+        timePicker.hour = this.hour
+        timePicker.minute = this.minute
+
+        timePicker.setOnTimeChangedListener { timePicker, hour, minute ->
+            this@DirectionsActivity.hour = hour
+            this@DirectionsActivity.minute = minute
+
+            currCalendar.set(this@DirectionsActivity.year, this@DirectionsActivity.month, this@DirectionsActivity.day, hour, minute)
+
+            this@DirectionsActivity.timestamp = currCalendar.getTimeInMillis()
+
+            showUserSelectDateTime()
+        }
+
+        timePicker.setIs24HourView(true)
 
         initPlaces()
 
@@ -99,5 +157,13 @@ class DirectionsActivity : AppCompatActivity() {
         val placesClient = Places.createClient(this)
     }
 
+    fun showUserSelectDateTime(){
+        Log.i("PICKER", this.year.toString())
+        Log.i("PICKER", this.month.toString())
+        Log.i("PICKER", this.day.toString())
+        Log.i("PICKER", this.hour.toString())
+        Log.i("PICKER", this.minute.toString())
+        Log.i("TIMESTAMP", this.timestamp.toString())
+    }
 }
 
