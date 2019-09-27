0df2385abbb4170dd27f8d8c10e0c30d57bc1a54
lisajhaaas <lisa_johanna.haas@smail.th-koeln.de>
2019-09-27 13:50:26 +0200

    login progress bar

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/login/Login.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/login/Login.kt
@@ -1,13 +1,50 @@
 package de.trabit.reportApp.login
 
+import android.animation.ValueAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.renderscript.ScriptGroup
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.*
 import de.trabit.reportApp.R
+import de.trabit.reportApp.reports.display.OverviewActivity
+
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //set progressbar animation
+
+        val signInBtn = findViewById(R.id.btn_next) as RelativeLayout
+        val progressBar = findViewById(R.id.progressBar) as ProgressBar
+        val signInText = findViewById(R.id.signInText) as TextView
+        val checkImage = findViewById(R.id.validationIcon) as ImageView
+
+        signInText.visibility = View.VISIBLE
+
+        signInBtn.setOnClickListener() {
+
+            progressBar.visibility = View.VISIBLE
+            signInText.setVisibility(View.GONE)
+
+            Handler().postDelayed(Runnable(){
+                run(){
+
+                    val LoginIntent = Intent (this,
+                        OverviewActivity::class.java)
+                    startActivity(LoginIntent)
+                    progressBar.setVisibility(View.GONE)
+                    checkImage.visibility = View.VISIBLE
+                }
+            },3100)
+
+        }
+
     }
 }
