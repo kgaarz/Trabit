7b3c1feccb2ce9e2653cd7eede7bcf533bf9ee6b
Josina Zotzmann <josina.zotzmann@smail.th-koeln.de>
2019-09-28 23:10:55 +0200

    add intent to EditProfile activity

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/EditProfile.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/EditProfile.kt
@@ -3,6 +3,7 @@ package de.trabit.reportApp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import de.trabit.reportApp.user.profile.ProfileActivity
 
@@ -12,11 +13,20 @@ class EditProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
-
+        //go back to profile screen
         val backToProfile = findViewById<ImageButton>(R.id.btn_back)
         backToProfile.setOnClickListener {
             val backProfileIntent = Intent(this, ProfileActivity::class.java)
             startActivity(backProfileIntent)
         }
+
+        //go back to profile screen after saving new settings
+        val backAfterSave = findViewById<Button>(R.id.profile_send_button)
+        backAfterSave.setOnClickListener {
+            val backAfterSaveIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(backAfterSaveIntent)
+        }
+
+        }
     }
-}
+
