882122e402c3998c7cfd099af0438145f1792b27
kgaarz <konstantin.gaarz@smail.th-koeln.de>
2019-09-29 22:54:35 +0200

    Funktion für das generelle Ausführen von POST-Requests hinzugefügt und in DirectionSelectionActivity eingebunden

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/DirectionSelectionActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/DirectionSelectionActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import de.trabit.directionsApp.requestHelper.RequestActivity
+import de.trabit.reportApp.BuildConfig
 import de.trabit.reportApp.R
+import org.json.JSONArray
+import org.json.JSONObject
+
+
+
 
 
 class DirectionSelectionActivity : AppCompatActivity() {
@@ -22,14 +28,52 @@ class DirectionSelectionActivity : AppCompatActivity() {
         val destinationLat = extras.getDouble("DESTINATION_LAT")
         val destinationLong = extras.getDouble("DESTINATION_LONG")
 
+        var origin = JSONObject()
+        origin.put("lat", originLat)
+        origin.put("lng", originLong)
+
+        var destination = JSONObject()
+        destination.put("lat", destinationLat)
+        destination.put("lng", destinationLong)
+
         val text = findViewById<TextView>(R.id.textView)
         text.setText(timestamp.toString() + " " + originLat.toString() + " " + originLong.toString() + " " + destinationLat.toString() + " " + destinationLong.toString())
 
-        val asyncTask = RequestActivity.GetRequest(getApplicationContext(), "traffics", "5d5414fa043e699565a4795e")
-        asyncTask.execute()
 
-        val jsonData = asyncTask.get().get("message")
+        //POST Mobilities with current location
+        var requestBodyM = JSONObject()
+        requestBodyM.put("lat", 50.941357)
+        requestBodyM.put("lng", 6.958307)
+        requestBodyM.put("radius", 2000)
 
-        Log.i("TAG", jsonData.toString())
+
+        val asyncPostForMobilites = RequestActivity.PostRequestWithOneID(getApplicationContext(),"mobilities", BuildConfig.DEMO_USERID_MONGODB , requestBodyM.toString())
+        asyncPostForMobilites.execute()
+        Log.i("TAG", asyncPostForMobilites.get().toString())
+
+
+        //POST DirectionsSelections with MobilitesID
+        var requestBodyDS = JSONObject()
+        requestBodyDS.put("origin", origin)
+        requestBodyDS.put("destination", destination)
+        requestBodyDS.put("departureTime", timestamp)
+
+
+        val asyncPost = RequestActivity.PostRequest(getApplicationContext(),"directionsSelections", BuildConfig.DEMO_USERID_MONGODB , asyncPostForMobilites.get().toString(), requestBodyDS.toString())
+        asyncPost.execute()
+        Log.i("TAG", asyncPost.get().toString())
+
+
+        val asyncGet2 = RequestActivity.GetRequest(getApplicationContext(), "directionsSelections", asyncPost.get().toString())
+        asyncGet2.execute()
+
+        var selectionOne: JSONObject = asyncGet2.get().getJSONObject("data").getJSONArray("selections").getJSONArray(0).getJSONObject(0)
+        var selectionTwo: JSONObject = asyncGet2.get().getJSONObject("data").getJSONArray("selections").getJSONArray(0).getJSONObject(1)
+
+        Log.i("TAG", selectionOne.toString())
+        Log.i("TAG", selectionTwo.toString())
+
+
+
     }
 }
--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/requestHelper/RequestActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/requestHelper/RequestActivity.kt
@@ -2,10 +2,16 @@ package de.trabit.directionsApp.requestHelper
 
 import android.content.Context
 import android.os.AsyncTask
-import android.util.Log
 import de.trabit.reportApp.BuildConfig
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import org.json.JSONObject
+import okhttp3.RequestBody.Companion.toRequestBody
+
+
+
+
+
 
 
 
@@ -34,13 +40,14 @@ class RequestActivity {
         }
     }
 
-    class PostRequest(context: Context, path: String, userId: String, id: String, requestBody: JSONObject): AsyncTask<Void, Void, JSONObject>(){
+    class PostRequest(context: Context, path: String, userId: String, id: String, requestBody: String): AsyncTask<Void, Void, String>(){
+        val JSON = "application/json; charset=utf-8".toMediaType()
 
         private var currentContext: Context? = null
         private var currentPath: String = ""
         private var currentUserId: String = ""
         private var currentId: String = ""
-        private var currentRequestBody: JSONObject? = null
+        private var currentRequestBody: String? = ""
 
         init {
             currentPath = path
@@ -50,14 +57,40 @@ class RequestActivity {
             currentRequestBody = requestBody
         }
 
-        override fun doInBackground(vararg params: Void?): JSONObject {
+        override fun doInBackground(vararg params: Void?): String? {
+            var body = currentRequestBody?.toRequestBody(JSON)
             val client = OkHttpClient()
-            val url = BuildConfig.DIRECTIONSAPI_BASE_URL + currentPath + "/" + currentId
-            val request = okhttp3.Request.Builder().url(url).build()
-            val response = client.newCall(request).execute()
-            var stringData = response.body?.string()
-            val jsonData = JSONObject(stringData)
-            return jsonData
+            val url = BuildConfig.DIRECTIONSAPI_BASE_URL + currentPath + "/" + currentUserId + "/" + currentId
+            val request = body?.let { okhttp3.Request.Builder().url(url).post(it).build() }
+            val response = request?.let { client.newCall(it).execute() }
+            var stringData = response?.body?.string()
+            return stringData
+        }
+    }
+
+    class PostRequestWithOneID(context: Context, path: String, userId: String, requestBody: String): AsyncTask<Void, Void, String>(){
+        val JSON = "application/json; charset=utf-8".toMediaType()
+
+        private var currentContext: Context? = null
+        private var currentPath: String = ""
+        private var currentUserId: String = ""
+        private var currentRequestBody: String? = ""
+
+        init {
+            currentPath = path
+            currentContext = context
+            currentUserId = userId
+            currentRequestBody = requestBody
+        }
+
+        override fun doInBackground(vararg params: Void?): String? {
+            var body = currentRequestBody?.toRequestBody(JSON)
+            val client = OkHttpClient()
+            val url = BuildConfig.DIRECTIONSAPI_BASE_URL + currentPath + "/" + currentUserId
+            val request = body?.let { okhttp3.Request.Builder().url(url).post(it).build() }
+            val response = request?.let { client.newCall(it).execute() }
+            var stringData = response?.body?.string()
+            return stringData
         }
     }
 }
