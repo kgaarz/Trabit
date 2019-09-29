9d114a02f370147bef4b0d37d0044e5b9e2ddc70
Julia Pritzen <julia.pritzen@gmail.com>
2019-09-30 01:32:07 +0200

    implemented mobility put request, small adjustments to layout

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/user/profile/EditMobilityActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/user/profile/EditMobilityActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
 import com.android.volley.NetworkResponse
 import com.android.volley.RequestQueue
@@ -16,30 +15,28 @@ import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import de.trabit.reportApp.BuildConfig
 import de.trabit.reportApp.R
-import de.trabit.reportApp.user.model.Profile
-import kotlinx.android.synthetic.main.activity_edit_profile.*
+import de.trabit.reportApp.user.model.Mobility
+import kotlinx.android.synthetic.main.activity_edit_profile_mobilities.*
 import org.json.JSONException
 import org.json.JSONObject
 
-class EditProfileActivity : AppCompatActivity() {
+class EditMobilityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        setContentView(R.layout.activity_edit_profile_mobilities)
 
-        //get user profile data
-        val firstname = intent.getStringExtra("firstname")
-        val lastname = intent.getStringExtra("lastname")
-        val email = intent.getStringExtra("email")
-        val residence = intent.getStringExtra("residence")
+        //get user mobility data
+        val car = intent.getBooleanExtra("car", false)
+        val driversLicense = intent.getBooleanExtra("driversLicense", false)
+        val bike = intent.getBooleanExtra("bike", false)
+        val trainTicket = intent.getBooleanExtra("trainTicket", false)
+        val sharing = intent.getBooleanExtra("sharing", false)
 
-        val firstnameField = findViewById<EditText>(R.id.editName)
-        firstnameField.setText(firstname)
-        val lastnameField = findViewById<EditText>(R.id.editLastName)
-        lastnameField.setText(lastname)
-        val emailField = findViewById<EditText>(R.id.editMail)
-        emailField.setText(email)
-        val homeField = findViewById<EditText>(R.id.editHome)
-        homeField.setText(residence)
+        mob1.isChecked = car
+        mob2.isChecked = driversLicense
+        mob3.isChecked = bike
+        mob4.isChecked = trainTicket
+        mob5.isChecked = sharing
 
         //go back to profile screen
         val backToProfile = findViewById<ImageButton>(R.id.btn_back)
@@ -49,29 +46,29 @@ class EditProfileActivity : AppCompatActivity() {
         }
 
         //go back to profile screen after saving new settings
-        val backAfterSave = findViewById<Button>(R.id.profile_send_button)
+        val backAfterSave = findViewById<Button>(R.id.profileMob_send_button)
         backAfterSave.setOnClickListener {
-            putUserProfile(BuildConfig.DEMO_USERID, Profile(firstnameField.text.toString(),lastnameField.text.toString(), emailField.text.toString(), homeField.text.toString()))
+            putUserMobility(BuildConfig.DEMO_USERID, Mobility(mob1.isChecked, mob2.isChecked, mob3.isChecked, mob4.isChecked, mob5.isChecked))
         }
     }
 
-    private fun putUserProfile(userId: String, profile: Profile){
-        val requestUrl = BuildConfig.REPORTAPI_BASE_URL + "users/$userId/profile"
-        val profileObject = JSONObject(Gson().toJson(profile))
+    private fun putUserMobility(userId: String, mobility: Mobility){
+        val requestUrl = BuildConfig.REPORTAPI_BASE_URL + "users/$userId/mobility"
+        val mobilityObject = JSONObject(Gson().toJson(mobility))
         val mQueue: RequestQueue = Volley.newRequestQueue(this)
-        val request = object : JsonObjectRequest(Method.PUT, requestUrl, profileObject,
+        val request = object : JsonObjectRequest(Method.PUT, requestUrl, mobilityObject,
             Response.Listener {
                 try {
                     val saveProfileIntent = Intent(this, ProfileActivity::class.java)
-                    startActivity(saveProfileIntent.putExtra("profileSaved", true))
+                    startActivity(saveProfileIntent.putExtra("mobilitySaved", true))
                 } catch (e: JSONException) {
                     e.printStackTrace()
-                    ErrorSnackbar(Layout_profile_edit).show("Speichern der Änderungen fehlgeschlagen!")
+                    ErrorSnackbar(Layout_mobility_edit).show("Speichern der Änderungen fehlgeschlagen!")
                 }
             }, Response.ErrorListener {
                 it.printStackTrace()
                 val errorMsg = String(it.networkResponse.data, Charsets.UTF_8)
-                ErrorSnackbar(Layout_profile_edit).show(errorMsg)
+                ErrorSnackbar(Layout_mobility_edit).show(errorMsg)
             }) {
             override fun parseNetworkResponse(response : NetworkResponse) : Response<JSONObject> {
                 return if (response.data.isEmpty()) {
--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/user/profile/ProfileActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/reportApp/user/profile/ProfileActivity.kt
@@ -22,11 +22,18 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        // check if changes were made in previous screen
         val profileSaved = intent.getBooleanExtra("profileSaved", false)
+        val mobilitySaved = intent.getBooleanExtra("mobilitySaved", false)
 
         if (profileSaved) {
             SuccessSnackbar(Layout_profile).show("Änderungen am Profil wurden gespeichert!")
         }
+
+        if (mobilitySaved) {
+            SuccessSnackbar(Layout_profile).show("Änderungen an den Mobilitätsmöglichkeiten wurden gespeichert!")
+        }
+
         // get user data
         getUser(BuildConfig.DEMO_USERID)
 
@@ -45,8 +52,13 @@ class ProfileActivity : AppCompatActivity() {
         //click button to edit mobility settings
         val editMobilitiesButton = findViewById<ImageButton>(R.id.btnEdit2)
         editMobilitiesButton.setOnClickListener {
-            val editMobilities = Intent(this, EditProfileMobilities::class.java)
-            startActivity(editMobilities)
+            val editMobilities = Intent(this, EditMobilityActivity::class.java)
+            startActivity(editMobilities
+                .putExtra("car", mob1.isChecked)
+                .putExtra("driversLicense", mob2.isChecked)
+                .putExtra("bike", mob3.isChecked)
+                .putExtra("trainTicket", mob4.isChecked)
+                .putExtra("sharing", mob5.isChecked))
         }
     }
 
@@ -57,18 +69,22 @@ class ProfileActivity : AppCompatActivity() {
             Response.Listener {
                 val gson = GsonBuilder().create()
                 val user = gson.fromJson(it.toString(), User::class.java)
+                //set profile
                 nameProfile.text = user.username
                 txtFirstnameData.text = user.profile.firstname
                 txtLastnameData.text = user.profile.lastname
                 txtMailData.text = user.profile.email
                 txtHomeData.text = user.profile.residence
+                //set mobility
+                mob1.isChecked = user.availableMobilityOptions.car
+                mob2.isChecked = user.availableMobilityOptions.driversLicense
+                mob3.isChecked = user.availableMobilityOptions.bike
+                mob4.isChecked = user.availableMobilityOptions.trainTicket
+                mob5.isChecked = user.availableMobilityOptions.sharing
             }, Response.ErrorListener {
                 it.printStackTrace()
                 ErrorSnackbar(Layout_profile).show("Abrufen des Profils fehlgeschlagen!")
             })
         mQueue.add(request)
     }
-
-
-
 }
