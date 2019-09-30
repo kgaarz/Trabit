9941eea21d9c3083cd52f8f0a220c35c1f607987
kgaarz <konstantin.gaarz@smail.th-koeln.de>
2019-09-30 13:15:04 +0200

    Verbindung zwschen DirectionSelectionActivity & MapActivity hergestellt

--- TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/DirectionSelectionActivity.kt
+++ TeamUX/MVP/frontend/app/src/main/java/de/trabit/directionsApp/DirectionSelectionActivity.kt
@@ -1,11 +1,11 @@
 package de.trabit.directionsApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.RelativeLayout
 import android.widget.TextView
 import de.trabit.directionsApp.requestHelper.RequestActivity
 import de.trabit.reportApp.BuildConfig
@@ -19,11 +19,14 @@ import org.json.JSONObject
 
 
 
+
 class DirectionSelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_direction_selection)
+
+        val intentForMap = Intent(this, MapActivity::class.java)
 
         val modesTextView = findViewById<TextView>(R.id.modesInput1)
         val durationTextView = findViewById<TextView>(R.id.durationInput1)
@@ -62,6 +65,7 @@ class DirectionSelectionActivity : AppCompatActivity() {
         val asyncPostForMobilites = RequestActivity.PostRequestWithOneID(getApplicationContext(),"mobilities", BuildConfig.DEMO_USERID_MONGODB , requestBodyM.toString())
         asyncPostForMobilites.execute()
         Log.i("TAG", asyncPostForMobilites.get().toString())
+        val mobilitiesId = asyncPostForMobilites.get().toString()
 
 
         //POST DirectionsSelections with MobilitesID
@@ -74,13 +78,14 @@ class DirectionSelectionActivity : AppCompatActivity() {
         val asyncPost = RequestActivity.PostRequest(getApplicationContext(),"directionsSelections", BuildConfig.DEMO_USERID_MONGODB , asyncPostForMobilites.get().toString(), requestBodyDS.toString())
         asyncPost.execute()
         Log.i("TAG", asyncPost.get().toString())
+        val directionSelectionId = asyncPost.get().toString()
 
 
         val asyncGet2 = RequestActivity.GetRequest(getApplicationContext(), "directionsSelections", asyncPost.get().toString())
         asyncGet2.execute()
 
-        var selectionOne: JSONObject = asyncGet2.get().getJSONObject("data").getJSONArray("selections").getJSONArray(0).getJSONObject(0)
-        var selectionTwo: JSONObject = asyncGet2.get().getJSONObject("data").getJSONArray("selections").getJSONArray(0).getJSONObject(1)
+        var selectionOne: JSONObject = asyncGet2.get().getJSONObject("data").getJSONArray("selections").getJSONObject(0)
+        var selectionTwo: JSONObject = asyncGet2.get().getJSONObject("data").getJSONArray("selections").getJSONObject(1)
 
         val duration1: Int = selectionOne.getInt("duration") / 60
         val distance1: Int = selectionOne.getInt("distance") / 1000
@@ -105,9 +110,17 @@ class DirectionSelectionActivity : AppCompatActivity() {
             override fun onClick(v: View) {
                 var requestBodyD1 = JSONObject()
                 requestBodyD1.put("selection", 0)
-                val asyncPostForDirection1 = RequestActivity.PostRequest(getApplicationContext(),"directions", BuildConfig.DEMO_USERID_MONGODB , asyncPost.get().toString(), requestBodyD1.toString())
+                val asyncPostForDirection1 = RequestActivity.PostRequest(getApplicationContext(),"directions", BuildConfig.DEMO_USERID_MONGODB , directionSelectionId, requestBodyD1.toString())
                 asyncPostForDirection1.execute()
                 Log.i("TAG", asyncPostForDirection1.get().toString())
+
+                val extras = Bundle()
+                extras.putString("MOBILITIES_ID", mobilitiesId)
+                extras.putString("DIRECTIONS_ID", asyncPostForDirection1.get().toString())
+
+                intentForMap.putExtras(extras)
+
+                startActivity(intentForMap)
             }
         })
 
@@ -116,9 +129,17 @@ class DirectionSelectionActivity : AppCompatActivity() {
             override fun onClick(v: View) {
                 var requestBodyD2 = JSONObject()
                 requestBodyD2.put("selection", 1)
-                val asyncPostForDirection2 = RequestActivity.PostRequest(getApplicationContext(),"directions", BuildConfig.DEMO_USERID_MONGODB , asyncPost.get().toString(), requestBodyD2.toString())
+                val asyncPostForDirection2 = RequestActivity.PostRequest(getApplicationContext(),"directions", BuildConfig.DEMO_USERID_MONGODB , directionSelectionId, requestBodyD2.toString())
                 asyncPostForDirection2.execute()
                 Log.i("TAG", asyncPostForDirection2.get().toString())
+
+                val extras = Bundle()
+                extras.putString("MOBILITIES_ID", mobilitiesId)
+                extras.putString("DIRECTIONS_ID", asyncPostForDirection2.get().toString())
+
+                intentForMap.putExtras(extras)
+
+                startActivity(intentForMap)
             }
         })
 
